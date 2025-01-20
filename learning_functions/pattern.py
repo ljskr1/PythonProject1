c4b64cb2c8c054a7e347ab03c2a413ca74d38e16
ljskr1 <lijsikhrakar7@gmail.com>
2025-01-20 07:22:29 +0545

    updated pattern

--- learning_functions/pattern.py
+++ learning_functions/pattern.py
@@ -1,33 +1,69 @@
-"""
-create a saquare of 'n'
-create a hollow square of side 'n'
-create of rectangle of side 'n'
-create a hollow rectangle of side 'n'
-create a pyramid of row 'n'
-can you fuse 1,2,3,4
-
-"""
-
-# def pattern_square(size):
-#     for i in range(size):
-#         row = '*' * size
-#         print(row)
-#
-# pattern_square(4)
-
-
-def pattern_hollow_square(size):
-    for i in range(size):
-        if i == 0 or i ==  4:
-            row = '*' * size
+# 1. Create a square of side 'n'.
+# Example.
+# n = 4
+# ****
+# ****
+# ****
+# ****
+def pattern_unified(shape="square", length=4, breadth=None, hollow=False):
+    if shape == "square":
+        breadth = length  # If it's a square, breadth is the same as length
+    for i in range(breadth):
+        if hollow and i != 0 and i != breadth - 1:
+            row = '*' + " " * (length - 2) + '*'
         else:
-            row = '*' + " " * 3 + '*'
-
+            row = '*' * length
         print(row)
 
-pattern_hollow_square(5)
+pattern_unified(shape="square", length=4)
+
+# 2. Create a hollow Square of side 'n'.
+# Example.
+# n = 4
+# ****
+# *  *
+# *  *
+# ****
+pattern_unified(shape="square", length=5, hollow=True)
 
 
+# 3. Create a rectangle of side 'n'.
+# Example.
+# l = 4, b = 3
+# ****
+# ****
+# ****
+def pattern_rectangle(length,breadth):
+    for i in range(breadth):
+        row = '*' * length
+        print(row)
+
+pattern_rectangle(4,3)
+
+
+# 4. Create a Hollow Rectangle of side 'n'.
+# Example.
+# l=4, b=3
+# ****
+# *  *
+# ****
+def pattern_hollow_rectangle(l, b):
+    for i in range(b):
+        if i == 0 or i == b - 1:
+            row = '*' * l
+            print(row)
+        else:
+            row = '*' + " " * (l - 2) + '*'
+            print(row)
+pattern_hollow_rectangle(4,3)
+
+
+# 5. Create a pyramid of row 'n'.
+# Example.
+# row = 3
+#   *
+#  ***
+# *****
 def pyramid(row):
     def progression():
          return 1 + (row - 1) * 2
