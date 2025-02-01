c36e861c26cc08aa863fb4b81b14a66411cede37
ljskr1 <lijsikhrakar7@gmail.com>
2025-02-01 07:54:26 +0545

    new classwork

---
+++ learning_oops/vector3d.py
@@ -0,0 +1,18 @@
+import math
+
+from vector import Vector
+
+class Vector3D(Vector):
+    def __init__(self, x, y, z):
+        Vector.__init__(self, x, y)
+        self._z = z
+
+    def magnitude(self): #method over-riding
+        return math.sqrt(self._x**2 + self._y**2 + self._z**2)
+
+if __name__ == "__main__":
+    v = Vector3D(1, 2, 3)
+    v1 = Vector(1, 2)
+    print("3D:",v)
+    print("2D:",v1)
+
